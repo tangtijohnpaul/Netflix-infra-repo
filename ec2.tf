03d82ed0b7c17e6d3032e11702a444355088762f
Blandin Chufi <yaichufi@gmail.com>
2024-10-07 19:55:32 -0500

    modified and added files

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,12 @@
 #this file creates an ec2 instance
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myec2-1" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "myec2-1"
+  }
+}
